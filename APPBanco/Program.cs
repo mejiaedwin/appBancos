4ae6b2f7289f2b22e236faf17dd16af1ef64f734
mejiaedwin <mejia9314.g@gmail.com>
2022-08-29 21:36:49 -0500

    primero

---
+++ APPBanco/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Banco
+    {
+        public Banco()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+    }
+}
